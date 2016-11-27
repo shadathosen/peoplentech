d85e36458efb390b92087183d3bc3f99f803b98e
shoshen <shoshen11@gmail.com>
2016-11-27 10:58:32 -0500

    setup

---
+++ src/Saturday/House.java
@@ -0,0 +1,32 @@
+package Saturday;
+
+public class House {
+
+	// variables
+	public int address  = 10;
+	public String houseName;
+	
+	// methods
+	public void watchTvShows(){
+		System.out.println("Watching Game of Thrones");		
+	}
+	public void sleep(){
+		System.out.println("People are sleeping now");
+	}
+	
+	public int addHouseSize(int length, int height){
+		int total = length + height;
+		return total;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/Saturday/HouseTest.java
@@ -0,0 +1,22 @@
+package Saturday;
+
+public class HouseTest {
+
+	public static void main(String[]args){
+		House myHouse = new House();
+		myHouse.watchTvShows();
+		int totalSize = myHouse.addHouseSize(43, 54);
+		System.out.println(totalSize);
+		
+				
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
